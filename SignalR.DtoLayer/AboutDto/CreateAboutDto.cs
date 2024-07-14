637d04dd4ab387834c4db8007767a4a030b11127
Uğur Türkmen <trkmn.ugur.12@gmail.com>
2024-07-14 11:32:36 +0300

    Add project files.

---
+++ SignalR.DtoLayer/AboutDto/CreateAboutDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DtoLayer.AboutDto
+{
+    public class CreateAboutDto
+    {
+        //ID otomatik geleceği için ona gerek yok
+        public string ImageUrl { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}

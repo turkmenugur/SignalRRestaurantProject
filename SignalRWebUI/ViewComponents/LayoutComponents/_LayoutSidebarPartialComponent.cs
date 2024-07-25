9007ebb8626108a4fe53f660ee2831606a560f27
Uğur Türkmen <trkmn.ugur.12@gmail.com>
2024-07-25 20:12:54 +0300

    theme preparation

---
+++ SignalRWebUI/ViewComponents/LayoutComponents/_LayoutSidebarPartialComponent.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.ViewComponents.LayoutComponents
+{
+	public class _LayoutSidebarPartialComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke() {  return View(); }
+	}
+}

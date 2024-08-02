efd8193482f86c0a744769149214c10b07c4d6ef
Uğur Türkmen <trkmn.ugur.12@gmail.com>
2024-08-02 19:41:20 +0300

    SignalR library

---
+++ SignalRWebUI/Controllers/SignalRHubController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.Controllers
+{
+	public class SignalRHubController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+    }
+}

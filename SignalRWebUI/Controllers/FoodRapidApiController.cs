e4f49d6acbdb57937a625eb33bb78c3e29d0b0c5
UĞUR TÜRKMEN <trkmn.ugur.12@gmail.com>
2024-12-24 19:34:32 +0300

    World Cuisine Food Lists with Rapid Api

---
+++ SignalRWebUI/Controllers/FoodRapidApiController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.RapidApiDtos;
+
+namespace SignalRWebUI.Controllers
+{
+    public class FoodRapidApiController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes"),
+                Headers =
+    {
+        { "x-rapidapi-key", "930243ad24mshf288b17a982fcafp1541b0jsn4928a34df70a" },
+        { "x-rapidapi-host", "tasty.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                //var values = JsonConvert.DeserializeObject<List<ResultTastyApi>>(body);
+                //return View(values.ToList());
+                var root = JsonConvert.DeserializeObject<RootTastyApi>(body);
+                var values = root.Results;
+                return View(values.ToList());
+            }
+        }
+    }
+}
